527eedfe38f9205b8ee8d7d694d275421a8dcaae
yarik <yaroslavk@tallium.com>
2017-07-28 20:07:10 +0300

    printed gameplay started

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,18 +1,18 @@
 import scala.None
 import scala.util.Random.shuffle
+import scala.io._
 
 object Main extends App{
 
-  type Field = Vector[Option[Int]]
-
+  type Field = Vector[Int]
+  type Path = List[Int]
   def generateField: Field = {
     def l: Stream[Vector[Int]] = shuffle(1 to 15).toVector #:: l
     l.filter(valid)
-      .head
-      .map(Some(_)) :+ None
+      .head :+ 16
   }
 
-  def valid(field: Vector[Int]): Boolean =
+  def valid(field: Field): Boolean =
     inversions(field.toList) % 2 == 0
 
   def inversions(field: List[Int]): Int = field match {
@@ -20,7 +20,35 @@ object Main extends App{
     case x :: xs => xs.count(_ < x) + inversions(xs)
   }
 
-  val f = List(13, 2, 10, 3, 1, 12, 8, 4, 5, 9, 6, 15, 14, 11, 7)
-//  println(generateField)
-  println(valid(f.toVector))
+  def isSolved(field: Field): Boolean = {
+    field equals field.sorted
+  }
+
+  def toString(field: Field): String = {
+    field.grouped(4).map(_.mkString("\t")).mkString("\n")
+  }
+
+  def apply(field: Option[Field], move: Int): Option[Field] = {
+    println(toString(field.get))
+    if(move != 5)
+      field // here we can apply or not (if move is not valid)
+    else
+      None // here the game is solved
+  }
+
+  def game(field: Field) = {
+    if(isSolved(field))
+      println("CONGARTS")
+    else {
+
+    }
+  }
+  val startField = generateField
+  import scala.io._
+  def gamePlay(moves: Iterator[Int]): Option[Field] = {
+    moves.foldLeft(Some(startField): Option[Field])((f, n) => apply(f, n.toInt))
+  }
+
+  val t = gamePlay(Source.stdin.getLines().map(x => x.toInt))
+//  println(toString(generateField))
 }
