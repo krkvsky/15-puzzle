4754989dd565e2ff6d36b125568d11d00446167f
haulfield <haulfield@ex.ua>
2017-07-29 10:17:01 +0300

    ierarchy changed gameplay done testing start

---
+++ src/test/scala/Puzzle.scala
@@ -0,0 +1,22 @@
+import org.scalatest.{FlatSpec, WordSpec}
+
+class PuzzleSpec extends WordSpec{
+
+  "Field" must{
+    val field = Field.create()
+
+    "be printable" in {
+//      assert(field.toString.length > 0)
+
+    }
+
+  }
+//
+//  "Input" should {
+//
+//  }
+//
+//  "Gameplay" should {
+//
+//  }
+}
